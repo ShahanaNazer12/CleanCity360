b7dcfb5c4820b879469bd2bf1f62b66386630617
ShahanaNazer12 <shahananasarkk@gmail.com>
2026-03-05 21:51:57 +0530

    fix api url

--- frontend/axios.js
+++ frontend/axios.js
@@ -8,7 +8,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://cleancity360.onrender.com/api/v1"
+  baseURL: "https://cleancity360.onrender.com/api/v1",
+  withCredentials: true
 });
 
 export default instance;
