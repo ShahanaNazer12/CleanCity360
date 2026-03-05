923dae5e34aee7774c981cb1d346f44c91ca07ef
ShahanaNazer12 <shahananasarkk@gmail.com>
2026-03-05 21:27:38 +0530

    fix api url for production

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,6 +1,14 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//      baseURL:'http://localhost:4000/api/v1'
+// });
+// export default instance
+
 import axios from "axios";
 
 const instance = axios.create({
-     baseURL:'http://localhost:4000/api/v1'
+  baseURL: "https://cleancity360.onrender.com/api/v1"
 });
-export default instance
+
+export default instance;
