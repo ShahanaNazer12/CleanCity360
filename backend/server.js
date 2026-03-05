a2010d657a0a46939d82836d72d3ad1b5fa787be
ShahanaNazer12 <shahananasarkk@gmail.com>
2026-03-05 16:09:24 +0530

    fix port for render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
+// const app = require("./app")
+// const dotenv = require("dotenv")
+// const dbConnection = require("./config/dbConnection")
+// dotenv.config({path:"./config/config.env"})
+
+// dbConnection();
+
+
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`server is running on portnumber ${process.env.PORT}`)
+// })
+
+
 const app = require("./app")
 const dotenv = require("dotenv")
 const dbConnection = require("./config/dbConnection")
-dotenv.config({path:"./config/config.env"})
+
+dotenv.config({ path: "./config/config.env" })
 
 dbConnection();
 
+const PORT = process.env.PORT || 4000;
 
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on portnumber ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
